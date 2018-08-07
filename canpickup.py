9170e1ab97240712077185e12f00e17701c8edab
CONF124 <conf124@soton.ac.uk>
2018-08-07 10:12:27 +0100

    Initial Commit

---
+++ canpickup.py
@@ -0,0 +1,16 @@
+import time
+from robot import Robot
+
+def canHeld():
+	# if button pressed
+	#   wait 2 seconds
+	#   if button still pressed
+	#     return true
+	# return false
+
+def liftCan():
+	# Rotate the servo 90 degrees
+
+def main(myRobot):
+	if canHeld():
+		liftCan()
