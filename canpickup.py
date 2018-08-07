58d18a116d9f39f803022085d36a73957482756b
CONF124 <conf124@soton.ac.uk>
2018-08-07 13:50:40 +0100

    First version of canpickup.py

--- canpickup.py
+++ canpickup.py
@@ -1,16 +1,29 @@
 import time
-from robot import Robot
+from robot import PinMode
+
+PIN_SERVO = 0
+PIN_SWITCH = 1
 
 def canHeld():
-	# if button pressed
-	#   wait 2 seconds
-	#   if button still pressed
-	#     return true
-	# return false
+	if servo_board.gpios[PIN_SWITCH].read() == PinValue.LOW:
+		time.sleep(2)
+	    if servo_board.gpios[PIN_SWITCH].read() == PinValue.LOW:
+			return True
+	return False
 
-def liftCan():
-	# Rotate the servo 90 degrees
+def liftCan(servo):
+	servo.position = 1
 
 def main(myRobot):
-	if canHeld():
-		liftCan()
+	motor_board = myRobot.motor_board
+	servo_board = myRobot.servo_board
+	power_board = myRobot.power_board
+	
+	servo_board.gpios[PIN_SWITCH].mode = PinMode.INPUT_PULLUP
+	
+	servo = servo_board.servos[PIN_SERVO]
+	servo.position = 0
+	
+	while not canHeld():
+		continue
+	liftCan(servo)
