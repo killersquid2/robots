e34a0e591e1146c8a24bf90933a3949662bb01ee
CONF124 <conf124@soton.ac.uk>
2018-08-09 11:18:55 +0100

    Changed servo positions and timings to work with actual robot

--- canpickup.py
+++ canpickup.py
@@ -2,24 +2,24 @@ import time
 from robot import PinMode, PinValue
 
 PIN_SERVO = 15
-PIN_SWITCH = 2
+PIN_SWITCH = 'a0'
 
 def canHeld(servo_board):
-    if servo_board.gpios[PIN_SWITCH].read() == PinValue.LOW:
-        time.sleep(2)
-        if servo_board.gpios[PIN_SWITCH].read() == PinValue.LOW:
+    if servo_board.read_analogue[PIN_SWITCH] > 2.5:
+        time.sleep(0.5)
+        if servo_board.read_analogue[PIN_SWITCH] > 2.5:
             return True
     return False
 
 def liftCan(servo):
-    servo.position = 1
+    servo.position = 0
 
 def main(myRobot):
     servo_board = myRobot.servo_board
     servo_board.gpios[PIN_SWITCH].mode = PinMode.INPUT_PULLUP
     servo = servo_board.servos[PIN_SERVO]
 	
-    servo.position = 0
+    servo.position = 1
     
     while not canHeld(servo_board):
         continue
